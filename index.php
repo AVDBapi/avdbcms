abb6ff4a6d9cbed8efa0e3f3c3f5d1b4c22bd9c5
brevis-ng <brevisnguyen@gmail.com>
2024-03-20 23:58:05 +0800

    hate you

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
 	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 //set the environment to production after installation
-define('ENVIRONMENT', 'production');
+define('ENVIRONMENT', 'development');
 
 // we don't want to access the main project before installation. redirect to installation page
 if (ENVIRONMENT === 'development') {
