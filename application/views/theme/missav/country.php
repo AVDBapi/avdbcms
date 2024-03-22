c5e38b956d23172887790abb692892f1a18f9e00
brevis-ng <brevisnguyen@gmail.com>
2024-03-23 03:49:57 +0800

    missav: update star, country style

--- application/views/theme/missav/country.php
+++ application/views/theme/missav/country.php
@@ -1,64 +1,35 @@
-<section class="inner-banner-section banner-section bg-overlay-black <?php echo (ovoo_config('bg_img_disable')=='1')? '':'bg_img'; ?>">
-	<?php if($this->common_model->get_ads_status('country_header')=='1'): ?>
-	<!-- header ads -->
-	<div id="ads" style="padding: 20px 0px;text-align: center;">
-	    <div class="container">
-	        <div class="row">
-	            <div class="col-md-12 text-center">
-	                <?php echo $this->common_model->get_ads('country_header'); ?>
-	            </div>
-	        </div>
-	    </div>
-	</div>
-	<!-- header ads -->
-	<?php endif; ?>
-	<!-- Breadcrumb -->
-	<div id="title-bar">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 col-sm-6 col-xs-12">
-					<div class="page-title">
-						<h1 class="text-uppercase"><?php echo $country_name; ?> <?php echo trans('movies'); ?></h1>
+<section>
+	<?php if ($this->common_model->get_ads_status('country_header') == '1'): ?>
+		<!-- header ads -->
+		<div id="ads" style="padding: 20px 0px;text-align: center;">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<?php echo $this->common_model->get_ads('country_header'); ?>
 					</div>
-				</div>
-				<div class="col-md-6 col-sm-6 col-xs-12 text-right">
-					<ul class="breadcrumb">
-					    <li>
-					    	<a href="<?php echo base_url();?>"><i class="fi ion-ios-home"></i><?php echo trans('home'); ?></a>
-					    </li>
-					    <li><?php echo trans('country'); ?></li>
-					    <li class="active"><?php echo $country_name; ?></li>
-					</ul>
 				</div>
 			</div>
 		</div>
-	</div>
-	<!-- End Breadcrumb -->
-		
+		<!-- header ads -->
+	<?php endif; ?>
+
 	<!-- Secondary Section -->
 	<div id="section-opt">
-	    <div class="container">
-	        <div class="row">
-	        <?php if ($total_rows>0):?>
-	            <!-- All Movies -->
-	            <div class="col-md-12 col-sm-12">
-	                <div class="latest-movie movie-opt">
-	                    <div class="row clean-preset">
-	                        <div class="movie-container">
-	                            <?php foreach ($all_published_videos as $videos): ?>
-	                            <div class="col-md-2 col-sm-3 col-xs-6">
-	                                <?php include('thumbnail.php'); ?>
-	                            </div>
-	                            <?php endforeach; ?>
-	                        </div>
-	                    </div>
-	                </div>
-	            </div>
-	            <!-- End All Movies -->
-				<?php else: echo "<h3 class='text-center text-uppercase'>No movie found by Country:  ".$country_name."</h3>"; endif; ?>
-	        </div>
-			<?php if($total_rows > 24): echo $links;endif;?>								
+		<h3 class="text-xl md:text-3xl font-semibold mb-3 md:mb-6 text-center"><?php echo "Watch ". $country_name . " movies and series" ?></h3>
+		<div class="">
+			<?php if ($total_rows > 0): ?>
+				<!-- All Movies -->
+				<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-5">
+					<?php foreach ($all_published_videos as $videos) : ?>
+						<?php include ('thumbnail.php'); ?>
+					<?php endforeach; ?>
+				</div>
+				<!-- End All Movies -->
+			<?php else:
+				echo "<h3 class='text-center text-uppercase'>No movie found by Country:  " . $country_name . "</h3>"; endif; ?>
 		</div>
+		<?php if ($total_rows > 24):
+			echo $links; endif; ?>
 	</div>
 	<!-- Secondary Section -->
 </section>
